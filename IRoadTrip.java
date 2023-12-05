5fd2062fc609c8d5a910b20f37a97c73dcb47850
Ahuynh111 <ahuynh8@dons.usfca.edu>
2023-12-05 13:31:34 -0800

    adding in name different cases to match borders.txt official name

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -47,15 +47,17 @@ public class IRoadTrip {
             File stateFile = new File(args[2]);
             Scanner stateScanner = new Scanner(stateFile);
 
-            createMap(borderScanner, capDistScanner, stateScanner);
+            findNeighborsList(borderScanner, createCountryList(stateScanner));
 
         }catch (FileNotFoundException FNFE){
             System.out.println("ERROR: Please enter the following required files - 'borders.txt' 'capdist.csv' 'state_name.tsv'");
             System.exit(0);
         }
+
+
     }
 
-    //CREATE GRAPH OF COUNTRIES//
+    //CREATE LIST OF COUNTRIES//
     public Country createCountryList (Scanner states){
         Country headCountry = new Country();
         Country pointerCountry  = new Country();
@@ -67,24 +69,173 @@ public class IRoadTrip {
                 String line = states.nextLine();
                 String[] splitLine = line.split("\t");
                 if (splitLine[4].equals("2020-12-31")){
-                    Country c = new Country(splitLine[0], splitLine[1], splitLine[2]);
-                    headCountry = c;
-                    pointerCountry = c;
-                    totalCountries++;
+                    if(splitLine[1].equals("USA")){
+                        Country c = new Country(splitLine[0], splitLine[1], "United States");
+                        headCountry = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else{
+                        Country c = new Country(splitLine[0], splitLine[1], splitLine[2]);
+                        headCountry = c;
+                        pointerCountry = c;
+                        totalCountries++;                        
+                    }
                 }
             }else{
                 String line = states.nextLine();
                 String[] splitLine = line.split("\t");
                 if (splitLine[4].equals("2020-12-31")){
-                    Country c = new Country(splitLine[0], splitLine[1], splitLine[2]);
-                    pointerCountry.next = c;
-                    pointerCountry = c;
-                    totalCountries++;
+                    if(splitLine[1].equals("USA")){
+                        Country c = new Country(splitLine[0], splitLine[1], "United States");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("TAZ")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Tanzania");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("DRV")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Vietnam");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("PRK")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Korea, North");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("ROK")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Korea, South");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("MYA")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Burma");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("GFR")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Germany");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("CZR")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Czechia");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("ITA")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Italy");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("MAC")){
+                        Country c = new Country(splitLine[0], splitLine[1], "North Macedonia");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("BOS")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Bosnia and Herzegovina");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++; 
+                    }else if(splitLine[1].equals("RUS")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Russia");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("BLR")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Belarus");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++; 
+                    }else if(splitLine[1].equals("CAP")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Cabo Verde");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++; 
+                    }else if(splitLine[1].equals("EQG")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Equatorial Guinea");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("CDI")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Cote d'Ivoire");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++; 
+                    }else if(splitLine[1].equals("GAM")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Gambia, The");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("GUI")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Guinea");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++; 
+                    }else if(splitLine[1].equals("BFO")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Burkina Faso");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("DRC")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Democratic Republic of the Congo");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++; 
+                    }else if(splitLine[1].equals("ZIM")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Zimbabwe");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("IRN")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Iran");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("TUR")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Turkey");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("YEM")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Yemen");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("KYR")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Kyrgyzstan");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("SRI")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Sri Lanka");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("CAM")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Cambodia");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else if(splitLine[1].equals("ETM")){
+                        Country c = new Country(splitLine[0], splitLine[1], "Timor-Leste");
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }else{
+                        Country c = new Country(splitLine[0], splitLine[1], splitLine[2]);
+                        pointerCountry.next = c;
+                        pointerCountry = c;
+                        totalCountries++;
+                    }
                 }
             }
         }
 
-        /* print list of countries
+        /*print list of countries
         pointerCountry = headCountry;
         while(pointerCountry.next != null){
             System.out.println(pointerCountry.Name);
@@ -92,14 +243,28 @@ public class IRoadTrip {
         }*/
 
         return headCountry;
+
     }
 
-    public void findNeighbors(Scanner borders, Country headCountry){
-        String neighbors = borders.nextLine();
-        String[] splitNeighbors = neighbors.split("[=;]");
-        
-        
+    public void findNeighborsList(Scanner borders, Country headCountry){
+        Country pointerCountry = headCountry;
 
+        while(pointerCountry.next != null){
+            while(borders.hasNextLine()){
+                Scanner borderingNeighbors = borders;
+                String neighbors = borders.nextLine();
+                String[] splitNeighbors = neighbors.split("[=;]");
+                //retrive all neighbors
+                if(splitNeighbors[0].trim().equals(pointerCountry.Name)){
+                    System.out.println("TARGET: " + pointerCountry.Name);
+                    System.out.println("FOUND IN LIST: " + splitNeighbors[0].trim());
+                    for(int i = 0; i < splitNeighbors.length; i++){
+                        System.out.println(splitNeighbors[i].trim());
+                    }
+                }
+            }
+            pointerCountry = pointerCountry.next;
+        }
 
     }
 
